5a53d8e4143b668ee1159de5e232ad5bd39d36c9
AdamGao <solonot@163.com>
2021-03-25 22:02:08 +0800

    简单实现了一个随机串生成算法。

--- snowflake_test.go
+++ snowflake_test.go
@@ -3,12 +3,17 @@ package ugener
 import "testing"
 
 func TestSnowflake(t *testing.T) {
-	t.Log(Snowflake.GetStringHex())
+	for i := 0; i < 10; i++ {
+		id := RandomWorker().GetId()
+		output := Short63(id)
+		t.Log(output)
+	}
+
 }
 
 func TestHexSnowflake(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		worker := RandomWorker()
-		t.Log(worker.WorkerId(), RandomWorker().GetStringHex())
+		t.Log(worker.WorkerId(), RandomWorker().GetStringHexTrim())
 	}
 }
